c46b87108e6f9de180ae15e73513b7b599376239
RR-08 <triya1315@gmail.com>
2021-01-17 13:09:27 +0530

    inital commit

---
+++ 3darray.c
@@ -0,0 +1,39 @@
+//1000 element array
+#include<stdio.h>
+#include<stdlib.h>
+
+int array[10][10][10],a,b,c;
+
+int main(void){
+
+    for(a=0;a<10;a++)
+    {
+        for (b = 0; b < 10; b++)
+        {
+            for(c=0;c<10;c++)
+            {
+                array[a][b][c]=rand();
+
+            }
+        }
+        
+    }
+
+        for(a=0;a<10;a++)
+    {
+        for (b = 0; b < 10; b++)
+        {
+            for(c=0;c<10;c++)
+            {
+                printf("array[%d][%d][%d]=%d\n",a,b,c,array[a][b][c]);
+
+            }
+            printf("press enter to continue and ctrl c for quiting");
+
+            getchar();
+        }
+        
+    }
+
+    return 0;
+}
